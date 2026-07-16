9a4e91e9fddaf403680252e0876e3f6597c37d80
Pedro-LM1 <pedro.lucasmf1@gmail.com>
2026-07-16 17:00:24 -0300

    Update hero names and levels in Main class

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+public class Main{
+    public static void main(String[] args){
+        Personagem heroi1 = new Personagem("Pedro", "Paladin", 8, 100, 11.5);
+        Personagem heroi2 = new Personagem("Arthus", "Guerreiro", 10, 100, 10.5);
 
-        .
-        .
-        .
-
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
